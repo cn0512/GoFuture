a3cda459a6d07354cfd05a4eb418403e997729bb
cn0512@126.com <cn0512@126.com>
2018-03-30 13:47:46 +0800

    influx

--- Instance/Makefile.bat
+++ Instance/Makefile.bat
@@ -1,5 +1,5 @@
 #make core
-go build -o ./bin/influx.exe ./influxdb/influx.go
+go build -o ./bin/influx.exe ./influxdb/
 go build -o ./bin/chat.exe ./chat/Server/
 go build -o ./bin/robot.exe ./chat/robot/
 
--- Instance/influxdb/influx.go
+++ Instance/influxdb/influx.go
@@ -10,6 +10,7 @@ import (
 	"os/signal"
 
 	cons "github.com/cn0512/GoFuture/constant"
+	influx "github.com/cn0512/GoServerFrame/Core/DB/InfluxDB"
 	mq "github.com/cn0512/GoServerFrame/Core/MQ/Redis"
 )
 
@@ -20,6 +21,8 @@ func (m *MsgCmd) Call(msg string) {
 	fmt.Println("Call:", msg)
 }
 
+var c *influx.InfluxClient
+
 func main() {
 	//sub mq
 	sub, err_sub := mq.NewSub(cons.Topic_Quote_Tick, &MsgCmd{})
@@ -27,6 +30,14 @@ func main() {
 		panic(err_sub)
 	}
 	defer sub.Shutdown()
+
+	//influx
+	//uri := fmt.Sprintf("http://%s:%d", "localhost", 8086)
+	cfg := &ServerCfg{}
+	Parse("./influx.yaml", cfg)
+	c = influx.New(cfg.Addr)
+	c.Ping()
+	c.Query()
 
 	qt := make(chan os.Signal, 1)
 	signal.Notify(qt, os.Interrupt, os.Kill)
